5bd59f9d480bccd9c25242762bb5afcce4f0dea5
Forairaaaaa <64732721+Forairaaaaa@users.noreply.github.com>
2022-08-10 19:00:45 +0800

    add output settings

--- src/AXP2101.cpp
+++ src/AXP2101.cpp
@@ -1,6 +1,41 @@
 #include "AXP2101.h"
 
 
+
+/**
+ * @brief Return min input
+ * 
+ * @param a 
+ * @param b 
+ * @return uint16_t 
+ */
+inline uint16_t AXP2101::_getMin(uint16_t a, uint16_t b) {
+    return ((a)<(b)?(a):(b));
+}
+
+/**
+ * @brief Return max input
+ * 
+ * @param a 
+ * @param b 
+ * @return uint16_t 
+ */
+inline uint16_t AXP2101::_getMax(uint16_t a, uint16_t b) {
+    return ((a)>(b)?(a):(b));
+}
+
+/**
+ * @brief Return limited input
+ * 
+ * @param input 
+ * @param min 
+ * @param max 
+ * @return uint16_t 
+ */
+uint16_t AXP2101::_getMid(uint16_t input, uint16_t min, uint16_t max) {
+    return _getMax(_getMin(input, max), min);
+}
+
 /**
  * @brief Init AXP2101
  * 
@@ -19,6 +54,52 @@ bool AXP2101::begin(TwoWire* wire) {
  * 
  */
 void AXP2101::powerOff() {
-    _I2C_write1Byte(0x10, (_I2C_read8Bit(0x10) | 0B00000001));
+    _I2C_write1Byte(AXP2101_REG_COM_CONFIG, (_I2C_read8Bit(AXP2101_REG_COM_CONFIG) | 0B00000001));
+}
+
+
+/**
+ * @brief Set output channel ON/OFF
+ * 
+ * @param channel 
+ * @param state true: ON / false: OFF
+ */
+void AXP2101::setOutputState(OUTPUT_CHANNEL channel, bool state) {
+    /* DCCDs */
+    if (channel < ALDO1) {
+        uint8_t buff = _I2C_read8Bit(AXP2101_REG_DCDC_ONOFF);
+        buff = state ? ( buff | (1U << channel) ) : ( buff & ~(1U << channel) );
+        _I2C_write1Byte(AXP2101_REG_DCDC_ONOFF, buff);
+    }
+    /* LDOs */
+    else {
+        uint8_t buff = _I2C_read8Bit(AXP2101_REG_LDO_ONOFF);
+        buff = state ? ( buff | (1U << (channel - ALDO1)) ) : ( buff & ~(1U << (channel - ALDO1)) );
+        _I2C_write1Byte(AXP2101_REG_LDO_ONOFF, buff);
+    }
+}
+
+
+/**
+ * @brief Set output channel's voltage
+ * 
+ * @param channel 
+ * @param voltage LDO: 500~3500 mA
+ */
+void AXP2101::setOutputVoltage(OUTPUT_CHANNEL channel, uint16_t voltage) {
+    /* DCDCs */
+    if (channel < ALDO1) {
+
+    }
+    /* LDOs */  
+    else {
+        /* Map voltage to 0~30 */
+        voltage = (_getMid(voltage, 500, 3500) / 100) - 5;
+        /* Get REG address */
+        uint8_t addr = channel + 142;
+        uint8_t buff = _I2C_read8Bit(addr);
+        buff = (buff & 0B11100000) | (voltage & 0B00011111);
+        _I2C_write1Byte(addr, buff);
+    }
 }
 
--- src/AXP2101.h
+++ src/AXP2101.h
@@ -16,17 +16,50 @@
 #include "Wire.h"
 #include "i2c_port/i2c_port.h"
 
-/* Defines */
-#define AXP2101_ADDR         0x34
-#define AXP2101_ADDR_READ    0x69
-#define AXP2101_ADDR_WRITE   0x68
+/* Device address */
+#define AXP2101_ADDR            0x34
+#define AXP2101_ADDR_READ       0x69
+#define AXP2101_ADDR_WRITE      0x68
+/* Internal REG address */
+#define AXP2101_REG_COM_CONFIG  0x10
+#define AXP2101_REG_DCDC_ONOFF  0x80
+#define AXP2101_REG_DCDC1_VSET  0x82
+#define AXP2101_REG_DCDC2_VSET  0x83
+#define AXP2101_REG_LDO_ONOFF   0x90
+#define AXP2101_REG_ALDO1_VSET  0x92
+#define AXP2101_REG_ALDO2_VSET  0x93
+#define AXP2101_REG_ALDO3_VSET  0x94
+#define AXP2101_REG_ALDO4_VSET  0x95
+#define AXP2101_REG_BLDO1_VSET  0x96
+#define AXP2101_REG_BLDO2_VSET  0x97
+
 
 /* AXP2101 class */
 class AXP2101 : public I2C_PORT {
     private:
+        uint16_t _getMin(uint16_t a, uint16_t b);
+        uint16_t _getMax(uint16_t a, uint16_t b);
+        uint16_t _getMid(uint16_t input, uint16_t min, uint16_t max);
     public:
-    bool begin(TwoWire* wire = &Wire);
-    void powerOff();
+        /* Type defines */
+        typedef enum {
+            DCDC1 = 0,
+            DCDC2,
+            DCDC3,
+            DCDC4,
+            ALDO1,
+            ALDO2,
+            ALDO3,
+            ALDO4,
+            BLDO1,
+            BLDO2,
+        } OUTPUT_CHANNEL;
+
+        /* Functions */
+        bool begin(TwoWire* wire = &Wire);
+        void powerOff();
+        void setOutputState(OUTPUT_CHANNEL channel, bool state);
+        void setOutputVoltage(OUTPUT_CHANNEL channel, uint16_t voltage);
 };
 
 
